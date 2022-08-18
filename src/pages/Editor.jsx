c6729e2d2dd09eab3c5a371eee22e3804ae5ab83
Moazam05 <salmanmoazam08@gmail.com>
2022-08-18 13:23:27 +0500

    predefined text

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -18,6 +18,17 @@ const Editor = () => {
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='App' title='Editor' />
       <RichTextEditorComponent>
+        <ul>
+          <li>
+            <p>Capable of handling markdown editing.</p>
+          </li>
+          <li>
+            <p>
+              Contains a modular library to load the necessary functionality on
+              demand.
+            </p>
+          </li>
+        </ul>
         <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
       </RichTextEditorComponent>
     </div>
