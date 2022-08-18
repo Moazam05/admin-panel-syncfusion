3a310bf2cb17e33c003c6b3a859eb3e8f63a625a
Moazam05 <salmanmoazam08@gmail.com>
2022-08-18 12:56:48 +0500

    kanban board

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,33 @@
+// React Imports
 import React from 'react';
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from '@syncfusion/ej2-react-kanban';
+// Custom Imports
+import Header from '../components/Header';
+// Dummy Data
+import { kanbanData, kanbanGrid } from '../data/dummy';
 
 const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Kanban' />
+      <KanbanComponent
+        id='Kanban'
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+        keyField='Status'
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
 };
 
 export default Kanban;
