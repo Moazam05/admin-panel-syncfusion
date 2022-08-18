eefe8941eafd78b8d385b8e5727b69a880a061cb
Moazam05 <salmanmoazam08@gmail.com>
2022-08-18 17:02:52 +0500

    Line Chart Area chart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,7 +1,46 @@
+// React Imports
 import React from 'react';
+// Syncfusion Imports
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  LineSeries,
+  Legend,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts';
+// Dummy Data
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from '../../data/dummy';
+// Context Provider
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[DateTime, Legend, Tooltip, LineSeries]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,18 @@
+// React Imports
 import React from 'react';
+// Custom Imports
+import Header from '../../components/Header';
+import LineChart from '../../components/Charts/LineChart';
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl '>
+      <Header category='Chart' title='Inflation' />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
